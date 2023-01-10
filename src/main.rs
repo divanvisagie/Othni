6f1517415b18c052326cd0bb72cf442589da3a1e
Divan Visagie <me@dvisagie.com>
2023-01-10 20:15:15 +0100

    Add rune data

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,41 @@
-use std::{ collections::BTreeMap};
-
 use eframe::egui;
 use egui::{FontData, FontFamily, FontDefinitions};
 
 
 //create a string array of runes
 const YOUNGER_FUTHARK: [&str; 16] = ["ᚠ","ᚢ","ᚦ", "ᚬ", "ᚱ", "ᚴ", "ᚼ", "ᚾ", "ᛁ", "ᛅ", "ᛋ", "ᛏ", "ᛒ", "ᛘ", "ᛚ", "ᛦ"];
-const LATIN_FUTHARK: [&str; 16] = ["f", "u", "þ", "ą", "r", "k", "h", "n", "i", "a", "s", "t", "b", "m", "I", "R"];
+const LATIN_REP: [&str; 16] = ["f", "u", "þ", "ą", "r", "k", "h", "n", "i", "a", "s", "t", "b", "m", "I", "R"];
+const FONT_KEY: &str = "my_font";
 
 fn main() {
-    let native_options = eframe::NativeOptions::default();
-    eframe::run_native("Othni", native_options, Box::new(|cc| Box::new(MyEguiApp::new(cc))));
+    let mut native_options = eframe::NativeOptions::default();
+    native_options.initial_window_size = Some(egui::vec2(800.0, 150.0));
+    eframe::run_native("Othni", native_options, Box::new(|cc| Box::new(AppState::new(cc))));
 }
 
 fn load_compatible_font () -> FontDefinitions {
     let mut fonts = FontDefinitions::default();
-    // Install my own font (maybe supporting non-latin characters):
-    fonts.font_data.insert("my_font".to_owned(),
+    // Install font to  support non-latin characters:
+    fonts.font_data.insert(FONT_KEY.to_owned(),
     FontData::from_static(include_bytes!("../fonts/segoe ui historic.ttf"))); // .ttf and .otf supported
 
     // Put my font first (highest priority):
     fonts.families.get_mut(&FontFamily::Proportional).unwrap()
-        .insert(0, "my_font".to_owned());
+        .insert(0, FONT_KEY.to_owned());
 
     // Put my font as last fallback for monospace:
     fonts.families.get_mut(&FontFamily::Monospace).unwrap()
-        .push("my_font".to_owned());
+        .push(FONT_KEY.to_owned());
     fonts
 }
 
 #[derive(Default)]
-struct MyEguiApp {
+struct AppState {
     latin_string: String,
     younger_futhark_string: String
 }
 
-impl MyEguiApp {
+impl AppState {
     fn new(cc: &eframe::CreationContext<'_>) -> Self {
         // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
         // Restore app state using cc.storage (requires the "persistence" feature).
@@ -43,19 +43,17 @@ impl MyEguiApp {
         // for e.g. egui::PaintCallback.
 
         // load font definition from File
-        
 
-        let mut fonts =  load_compatible_font();
+        let fonts =  load_compatible_font();
 
         cc.egui_ctx.set_fonts(fonts);
         Self::default()
     }
 }
 
-impl eframe::App for MyEguiApp {
-  
+impl eframe::App for AppState {
 
-   fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+   fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
        egui::CentralPanel::default().show(ctx, |ui| {
     
         ui.heading(self.latin_string.as_str());
@@ -68,15 +66,16 @@ impl eframe::App for MyEguiApp {
         ui.horizontal(|ui| {
             ui.label("Younger Futhark string:");
             ui.text_edit_singleline(&mut self.younger_futhark_string);
+        });
 
-
+        ui.horizontal(|ui| {
             // for each rune in RUNES
             for rune in YOUNGER_FUTHARK.iter() {
                 // create a button
                 if ui.button(*rune).clicked() {
                     // add character to younger_futhark_string
                     self.younger_futhark_string.push_str(rune);
-                    self.latin_string.push_str(LATIN_FUTHARK[YOUNGER_FUTHARK.iter().position(|&r| r == *rune).unwrap()]);
+                    self.latin_string.push_str(LATIN_REP[YOUNGER_FUTHARK.iter().position(|&r| r == *rune).unwrap()]);
                 }
             }
         });
